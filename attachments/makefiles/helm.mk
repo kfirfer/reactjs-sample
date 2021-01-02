7f9236769c3b986ed1aeeb7f0353a8072657d67f
Kfir <kfirfer@gmail.com>
2021-01-02 08:55:07 +0200

    wip

--- attachments/makefiles/helm.mk
+++ attachments/makefiles/helm.mk
@@ -5,4 +5,7 @@
 helm.charts.reactjs-sample.install: ##
 	helm upgrade -i reactjs-sample attachments/kubernetes/reactjs-sample --namespace reactjs-sample --create-namespace
 
+helm.charts.reactjs-sample.install.hardcoded: ##
+	helm upgrade -i reactjs-sample attachments/kubernetes/reactjs-sample --values attachments/kubernetes/reactjs-sample/values-custom.yaml --namespace reactjs-sample
+
 
